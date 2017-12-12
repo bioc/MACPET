86f354b06c6798d56efc7f98f762e2fa8651dd33
IoannisVardaxis <ioannis.vardaxis@ntnu.no>
2017-12-12 10:47:55 +0100

    Changes in the vignette

--- vignettes/MACPET.R
+++ vignettes/MACPET.R
@@ -3,174 +3,214 @@ BiocStyle::latex2
 
 ## ----eval=TRUE,echo=TRUE---------------------------------------------------
 #Create a temporary test folder, or anywhere you want:
-AnalysisDir=file.path(tempdir(),"MACPETtest")
-dir.create(AnalysisDir)#where you will save the results.
+SA_AnalysisDir=file.path(tempdir(),"MACPETtest")
+dir.create(SA_AnalysisDir)#where you will save the results.
 
 ## --------------------------------------------------------------------------
 library(MACPET)
 
 ## --------------------------------------------------------------------------
-load(system.file("extdata", "pselfData.rda", package = "MACPET"))
-class(pselfData) #example name
-pselfData #print method
+load(system.file("extdata", "MACPET_pselfData.rda", package = "MACPET"))
+class(MACPET_pselfData) #example name
+MACPET_pselfData #print method
 
 ## --------------------------------------------------------------------------
-metadata(pselfData)
+metadata(MACPET_pselfData)
 
 ## --------------------------------------------------------------------------
-seqinfo(pselfData)
+seqinfo(MACPET_pselfData)
 
 ## --------------------------------------------------------------------------
-load(system.file("extdata", "psfitData.rda", package = "MACPET"))
-class(psfitData) #example name
-psfitData #print method
+load(system.file("extdata", "MACPET_psfitData.rda", package = "MACPET"))
+class(MACPET_psfitData) #example name
+MACPET_psfitData #print method
 
 ## --------------------------------------------------------------------------
-head(metadata(psfitData)$Peaks.Info)
+head(metadata(MACPET_psfitData)$Peaks.Info)
 
 ## --------------------------------------------------------------------------
-load(system.file("extdata", "pinterData.rda", package = "MACPET"))
-class(pinterData) #example name
-pinterData #print method
+load(system.file("extdata", "MACPET_pinterData.rda", package = "MACPET"))
+class(MACPET_pinterData) #example name
+MACPET_pinterData #print method
 
 ## ----eval=TRUE-------------------------------------------------------------
-metadata(pinterData)
+metadata(MACPET_pinterData)
 
 ## --------------------------------------------------------------------------
-load(system.file("extdata", "pintraData.rda", package = "MACPET"))
-class(pintraData)#example name
-pintraData#print method
+load(system.file("extdata", "MACPET_pintraData.rda", package = "MACPET"))
+class(MACPET_pintraData)#example name
+MACPET_pintraData#print method
 
 ## --------------------------------------------------------------------------
-metadata(pintraData)
+metadata(MACPET_pintraData)
 
 ## --------------------------------------------------------------------------
-class(pselfData)
-summary(pselfData)
+class(MACPET_pselfData)
+summary(MACPET_pselfData)
 
 ## --------------------------------------------------------------------------
-class(psfitData)
-summary(psfitData)
+class(MACPET_psfitData)
+summary(MACPET_psfitData)
 
 ## --------------------------------------------------------------------------
-class(pintraData)
+class(MACPET_pintraData)
 requireNamespace("ggplot2")
 requireNamespace("reshape2")
-summary(pintraData,heatmap=TRUE)
+summary(MACPET_pintraData,heatmap=TRUE)
 
 ## --------------------------------------------------------------------------
-class(pinterData)
+class(MACPET_pinterData)
 requireNamespace("ggplot2")
 requireNamespace("reshape2")
-summary(pinterData,heatmap=TRUE)
+summary(MACPET_pinterData,heatmap=TRUE)
 
 ## --------------------------------------------------------------------------
 requireNamespace("ggplot2")
-class(pselfData)
+class(MACPET_pselfData)
 # PET counts plot
-plot(pselfData)
+plot(MACPET_pselfData)
 
 ## --------------------------------------------------------------------------
-class(psfitData)
+class(MACPET_psfitData)
 #binding site couts:
-plot(psfitData,kind="PeakCounts")
+plot(MACPET_psfitData,kind="PeakCounts")
 
 ## --------------------------------------------------------------------------
 # region example with binding sites:
-plot(psfitData,kind="PeakPETs",RegIndex=1)
+plot(MACPET_psfitData,kind="PeakPETs",RegIndex=1)
 
 ## --------------------------------------------------------------------------
-class(pintraData)
+class(MACPET_pintraData)
 #plot counts:
-plot(pintraData)
+plot(MACPET_pintraData)
 
 ## --------------------------------------------------------------------------
-class(pinterData)
+class(MACPET_pinterData)
 requireNamespace("igraph")
 #network plot:
-plot(pinterData)
+plot(MACPET_pinterData)
 
 ## ----eval=TRUE,echo=TRUE---------------------------------------------------
-class(psfitData)#PSFit class
-exportPeaks(object=psfitData,file.out="Peaks",threshold=1e-5,savedir=AnalysisDir)
+class(MACPET_psfitData)#PSFit class
+exportPeaks(object=MACPET_psfitData,file.out="Peaks",threshold=1e-5,savedir=SA_AnalysisDir)
 
 ## ----eval=TRUE,echo=TRUE---------------------------------------------------
-class(psfitData)#PSFit class
-object=PeaksToGRanges(object=psfitData,threshold=1e-5)
+class(MACPET_psfitData)#PSFit class
+object=PeaksToGRanges(object=MACPET_psfitData,threshold=1e-5)
 object
 
 ## ----eval=TRUE,echo=TRUE---------------------------------------------------
-class(psfitData)#PSFit class
-TagsToGInteractions(object=psfitData,threshold=1e-5)
+class(MACPET_psfitData)#PSFit class
+TagsToGInteractions(object=MACPET_psfitData,threshold=1e-5)
 
 
 ## ----eval=TRUE,echo=TRUE---------------------------------------------------
-class(psfitData)#PSFit class
-PeaksToNarrowPeak(object=psfitData,threshold=1e-5,
-                  file.out="MACPET_peaks.narrowPeak",savedir=AnalysisDir)
+class(MACPET_psfitData)#PSFit class
+PeaksToNarrowPeak(object=MACPET_psfitData,threshold=1e-5,
+                  file.out="MACPET_peaks.narrowPeak",savedir=SA_AnalysisDir)
 
 ## ----eval=TRUE,echo=TRUE---------------------------------------------------
  #--remove information and convert to GInteractions:
-object=pselfData
+object=MACPET_pselfData
+#--remove information and convert to GInteractions:
 S4Vectors::metadata(object)=list(NULL)
-class(object)="GInteractions"
-GenomePkg="BSgenome.Hsapiens.UCSC.hg19" #genome of the data.
-BlackList="hg19"
-object=ConvertToPSelf(object=object,GenomePkg=GenomePkg,BlackList=BlackList)
-class(object)
+class(object)='GInteractions'
+#----input parameters
+S2_BlackList=TRUE
+SA_prefix="MACPET"
+S2_AnalysisDir=SA_AnalysisDir
+
+ConvertToPSelf(object=object,
+               S2_BlackList=S2_BlackList,
+               SA_prefix=SA_prefix,
+               S2_AnalysisDir=S2_AnalysisDir)
+#load object:
+rm(MACPET_pselfData)#old object
+load(file.path(S2_AnalysisDir,"MACPET_pselfData"))
+class(MACPET_pselfData)
+
 
 ## ----echo=TRUE,eval=TRUE---------------------------------------------------
-AnalysisStatistics(x.self=pselfData,#One of the self-ligated classes.
-                    x.intra=pintraData,#NULL for not printing the class.
-                    x.inter=pinterData,#NULL for not printing the class.
-                    #specify a name for the output to be saved.
-                    file.out="Statistics",
-                    savedir=AnalysisDir,#Where to save the output.
-                    threshold=1e-5)#Theshold for FDR
+AnalysisStatistics(x.self=MACPET_psfitData,
+                   x.intra=MACPET_pintraData,
+                   x.inter=MACPET_pinterData,
+                   file.out='AnalysisStats',
+                   savedir=SA_AnalysisDir,
+                   threshold=1e-5)
 
 
 ## ----echo=TRUE,eval=TRUE---------------------------------------------------
- #load sample data to use in the algorithm and give inputs:
- DataDir=system.file("extdata", package = "MACPET") #data path
- DataFile="SampleChIAPETData.bam" #data name
- PopImage=TRUE #Sample data, not very good classifiction results.
- GenomePkg="BSgenome.Hsapiens.UCSC.hg19" #genome of the data.
- fileSelf="pselfData" #name for Self-ligated
- fileIntra="pintraData" #name for Intra-chromosomal
- fileInter="pinterData" #name for Inter-chromosomal
- BlackList="hg19" #remove PETs in black listed regions
- fileSelfFit="psfitData"
- method="BH"
- Stages=c(0:1)
+requireNamespace('ggplot2')
+
+#Create a temporary forder, or anywhere you want:
+S1_AnalysisDir=SA_AnalysisDir
+
+#directories of the BAM files:
+BAM_file_1=system.file('extdata', 'SampleChIAPETDataRead_1.bam', package = 'MACPET')
+BAM_file_2=system.file('extdata', 'SampleChIAPETDataRead_2.bam', package = 'MACPET')
+SA_prefix="MACPET"
+
+#convert to paired-end BAM:
+ConvertToPE_BAM(S1_AnalysisDir=S1_AnalysisDir,
+             SA_prefix=SA_prefix,
+             S1_BAMStream=2000000,S1_image=TRUE,
+             S1_genome="hg19",BAM_file_1=BAM_file_1,
+             BAM_file_2=BAM_file_2)
+
+#test if the resulted BAM is paired-end:
+PairedBAM=file.path(S1_AnalysisDir,paste(SA_prefix,"_Paired_end.bam",sep=""))
+Rsamtools::testPairedEndBam(file = PairedBAM, index = PairedBAM)
+
+bamfile = Rsamtools::BamFile(file = PairedBAM,asMates = TRUE)
+GenomicAlignments::readGAlignmentPairs(file = bamfile,use.names = FALSE,
+                                with.which_label = FALSE,
+                                strandMode = 1)
+
+
+## ----echo=TRUE,eval=TRUE---------------------------------------------------
+
+#give directory of the BAM file:
+S2_PairedEndBAMpath=system.file('extdata', 'SampleChIAPETData.bam', package = 'MACPET')
+
+#give prefix name:
+SA_prefix="MACPET"
 
 #parallel backhead can be created using the BiocParallel package
-# requireNamespace("BiocParallel")
-# snow <- BiocParallel::SnowParam(workers = 1, type = "SOCK", progressbar=FALSE)
-# BiocParallel::register(snow, default=TRUE)
+#parallel backhead can be created using the BiocParallel package
+#requireNamespace('BiocParallel')
+#snow <- BiocParallel::SnowParam(workers = 4, type = 'SOCK', progressbar=FALSE)
+#BiocParallel::register(snow, default=TRUE)
 
 #-run for the whole binding site analysis:
-PeakCallerUlt(DataDir=DataDir,
-           DataFile=DataFile,
-           AnalysisDir=AnalysisDir,
-           GenomePkg=GenomePkg,
-           fileSelf=fileSelf,
-           fileIntra=fileIntra,
-           fileInter=fileInter,
-           PopImage=PopImage,
-           fileSelfFit=fileSelfFit,
-           method=method,
-           BlackList=BlackList,
-           Stages=Stages)
-#load results:
-load(file.path(AnalysisDir,fileSelf))
-class(pselfData) # see methods for this class
-load(file.path(AnalysisDir,fileIntra))
-class(pintraData) # see methods for this class
-load(file.path(AnalysisDir,fileInter))
-class(pinterData) # see methods for this class
-load(file.path(AnalysisDir,fileSelfFit))
-class(psfitData) # see methods for this class
+MACPETUlt(SA_AnalysisDir=SA_AnalysisDir,
+       SA_stages=c(2:3),
+       SA_prefix=SA_prefix,
+       S2_PairedEndBAMpath=S2_PairedEndBAMpath,
+       S2_image=TRUE,
+       S2_BlackList=TRUE,
+       S3_image=TRUE)
 
+#load results:
+SelfObject=paste(SA_prefix,"_pselfData",sep="")
+load(file.path(SA_AnalysisDir,"S2_results",SelfObject))
+SelfObject=get(SelfObject)
+class(SelfObject) # see methods for this class
+
+IntraObject=paste(SA_prefix,"_pintraData",sep="")
+load(file.path(SA_AnalysisDir,"S2_results",IntraObject))
+IntraObject=get(IntraObject)
+class(IntraObject) # see methods for this class
+
+InterObject=paste(SA_prefix,"_pinterData",sep="")
+load(file.path(SA_AnalysisDir,"S2_results",InterObject))
+InterObject=get(InterObject)
+class(InterObject) # see methods for this class
+
+SelfFitObject=paste(SA_prefix,"_psfitData",sep="")
+load(file.path(SA_AnalysisDir,"S3_results",SelfFitObject))
+SelfFitObject=get(SelfFitObject)
+class(SelfFitObject) # see methods for this class
 #-----delete test directory:
-unlink(AnalysisDir,recursive=TRUE)
+unlink(SA_AnalysisDir,recursive=TRUE)
+
 
